9a920b1fc96b89596923df2af51dcc0d3ccde096
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-01-25 18:47:11 -0500

    mejorando

--- Front-End/src/environments/environment.prod.ts
+++ Front-End/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   versionModulo:'1.0.0',
-  dominio:"http://ciscunl.info:8080/bolsa_empleo"
+  dominio:"http://ciscunl.info:8080/bolsa_empleo1"
 };
